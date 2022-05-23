fc0633280123fd0e3e47ccabc15a25b0c2a834ff
rondinelli-oliveira <rondinelliads@gmail.com>
2022-05-23 00:25:07 -0300

    created api java.

---
+++ angular-spring-api/src/main/java/com/evoltion/angularspringapi/AngularSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.evoltion.angularspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringApiApplication.class, args);
+	}
+
+}
---
+++ angular-spring-api/src/test/java/com/evoltion/angularspringapi/AngularSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evoltion.angularspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
